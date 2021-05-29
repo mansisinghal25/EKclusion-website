954719da9da15d09163f567b5c542ee716932d47
ms1901 <manasvi19369@iiitd.ac.in>
2021-05-29 15:44:19 +0530

    Added quiz functionality

--- js/main.js
+++ js/main.js
@@ -124,3 +124,118 @@
     
 })(jQuery);
 
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
+
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.add('correct')
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+   nextButton.classList.remove('correct')
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+  nextButton.classList.add('correct')
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const questions = [
+  {
+    question: 'What is Monica\'s Apartment number?',
+    answers: [
+      { text: '20', correct: true },
+      { text: '21', correct: false }
+    ]
+  },
+  {
+    question: 'Who is the best Character in Friends?',
+    answers: [
+      { text: 'Chandler', correct: true },
+      { text: 'Monica', correct: true },
+      { text: 'Phoebe', correct: true },
+      { text: 'Joey', correct: true }
+    ]
+  },
+  {
+    question: 'Who is the best couple in Friends?',
+    answers: [
+      { text: 'Chandler-Monica', correct: true },
+      { text: 'Ross Rachel', correct: false },
+      { text: 'Joey Phoebe', correct: false },
+      { text: 'Monica Richard', correct: false }
+    ]
+  },
+  {
+    question: 'What is 4 * 2?',
+    answers: [
+      { text: '6', correct: false },
+      { text: '8', correct: true }
+    ]
+  }
+]
